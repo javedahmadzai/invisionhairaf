e37690cd691dee3d83bcaf878a3d62bfbdcd0658
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-04-08 22:39:19 +0500

    fixed uri

--- src/app/Middlewares/PagePath.php
+++ src/app/Middlewares/PagePath.php
@@ -9,10 +9,9 @@ class PagePath extends Middleware
 {
     public function __invoke(Request $request, Response $response, callable $next)
     {
-        $this->view->getEnvironment()->addGlobal('uri', trim(
-            $request->getUri()->getPath(),
-            DIRECTORY_SEPARATOR
-        ));
+        $uri = trim($request->getUri()->getPath(), DIRECTORY_SEPARATOR);
+
+        $this->view->getEnvironment()->addGlobal('uri', $uri ?? '/');
         return $next($request, $response);
     }
 }
